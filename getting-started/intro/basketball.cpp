283d28152f80ed0b65ec5c5c7afb18ded2dfb5c6
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2025-01-23 09:28:22 -0300

    [add] solution of Kattis task: Basketball

---
+++ getting-started/intro/basketball.cpp
@@ -0,0 +1,19 @@
+/*
+ * @author corlet1
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int INF = 1e9;
+const long long LLINF = 4e18;
+const double EPS = 1e-9;
+const int MAXN = 210;
+
+int main() {
+    char game[MAXN];
+    scanf("%s\n", game);
+    printf("%c\n", game[strlen(game) - 2]);
+    return 0;
+}
+
